2452a443fb6d82798c7ca79016f3805a066149e7
Japjeet <japjeet88@gmail.com>
2021-01-17 11:46:42 +1300

    this for primitive data types

---
+++ src/primitivedatatype/Order.java
@@ -0,0 +1,25 @@
+package primitivedatatype;
+
+public class Order {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int orderId = 123;
+		System.out.println("Order Ids - " + orderId);
+		
+		float price = 10.20f;
+		System.out.println("Price - " + price);
+		
+		boolean myFlag = true;
+		System.out.println("my Flag - " + myFlag);
+		
+		long myName = 150000000;
+		System.out.println("my Flag - " + myName);
+		
+		myFlag = false;
+		System.out.println("My flag " + myFlag);
+		
+		}
+
+}
--- src/xyzpackage/XyzClass.java
+++ src/xyzpackage/XyzClass.java
@@ -5,6 +5,7 @@ public class XyzClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	
 	}
 
 }
